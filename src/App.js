2fee2b27dc8b672937d2255a4e2fd9239a96c12c
Anas azkoul <anas@Anass-MacBook-Air.local>
2022-08-01 16:57:17 +0200

    finished project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-function App() {
-  return (
-    <div className="App">
+import {useState} from 'react'
+import ReactMarkdown from 'react-markdown';
 
-    </div>
+
+function App () {
+  const [markdown, setMarkdown] = useState('## markdown preview'); 
+  return (
+    <main>
+      <section className="markdown">
+        <textarea
+          className='input'
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className="result">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
   );
 }
 
